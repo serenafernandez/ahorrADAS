02fdfd2125604899d311729deb770b1edddb1eb9
ivanasaenzs <84483098+ivanasaenzs@users.noreply.github.com>
2023-12-16 21:20:08 -0300

    added functionality to the navbar and different section buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
 // FUNCIONES AUXILIARES
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selectors) => document.querySelectorAll(selectors);
+
+// Funcionalidad secciones - botones
+const displaySection = (hiddenSection) => {
+  const sections = [
+    $("#balance-section"),
+    $("#categories-section"),
+    $("#edit-category-section"),
+    $("#reports-section"),
+    $("#new-operation-section"),
+    $("#edit-operation-section"),
+  ];
+
+  for (const section of sections) {
+    if (section === hiddenSection) {
+      section.classList.remove("hidden");
+    } else {
+      section.classList.add("hidden");
+    }
+  }
+};
+
+$("#balance-nav").addEventListener("click", () => {
+  displaySection($("#balance-section"));
+});
+
+$("#categories-nav").addEventListener("click", () => {
+  displaySection($("#categories-section"));
+});
+
+// $("#edit-category-button").addEventListener("click", () => {
+
+// });
+
+$("#reports-nav").addEventListener("click", () => {
+  displaySection($("#reports-section"));
+});
+
+$("#new-operation-button").addEventListener("click", () => {
+  displaySection($("#new-operation-section"));
+});
+
+$("#cancel-operation-button").addEventListener("click", () => {
+  $("#new-operation-section").classList.add("hidden");
+  $("#balance-section").classList.remove("hidden");
+});
+
+// Ocultar - mostrar filtros
+$("#hide-show-filters").addEventListener("click", () => {
+  if ($("#filters-content").classList.contains("hidden")) {
+    $("#filters-content").classList.remove("hidden");
+    $("#hide-show-filters").innerText = "Ocultar filtros";
+  } else {
+    $("#filters-content").classList.add("hidden");
+    $("#hide-show-filters").innerText = "Mostrar filtros";
+  }
+});
