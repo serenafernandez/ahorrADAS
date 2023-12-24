b0e73c80189834a4ca69001eab49d5f7cd95785f
ivanasaenzs <84483098+ivanasaenzs@users.noreply.github.com>
2023-12-24 19:14:42 -0300

    added more utility functions

--- js/main.js
+++ js/main.js
@@ -1,59 +1,24 @@
-// FUNCIONES AUXILIARES
+/* UTILITIES */
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selectors) => document.querySelectorAll(selectors);
 
-// Funcionalidad secciones - botones
-const displaySection = (hiddenSection) => {
-  const sections = [
-    $("#balance-section"),
-    $("#categories-section"),
-    $("#edit-category-section"),
-    $("#reports-section"),
-    $("#new-operation-section"),
-    $("#edit-operation-section"),
-  ];
-
-  for (const section of sections) {
-    if (section === hiddenSection) {
-      section.classList.remove("hidden");
-    } else {
-      section.classList.add("hidden");
-    }
+const showElement = (selectors) => {
+  for (const selector of selectors) {
+    $(selector).classList.remove("hidden");
   }
 };
 
-$("#balance-nav").addEventListener("click", () => {
-  displaySection($("#balance-section"));
-});
-
-$("#categories-nav").addEventListener("click", () => {
-  displaySection($("#categories-section"));
-});
-
-// $("#edit-category-button").addEventListener("click", () => {
-
-// });
-
-$("#reports-nav").addEventListener("click", () => {
-  displaySection($("#reports-section"));
-});
-
-$("#new-operation-button").addEventListener("click", () => {
-  displaySection($("#new-operation-section"));
-});
-
-$("#cancel-operation-button").addEventListener("click", () => {
-  $("#new-operation-section").classList.add("hidden");
-  $("#balance-section").classList.remove("hidden");
-});
-
-// Ocultar - mostrar filtros
-$("#hide-show-filters").addEventListener("click", () => {
-  if ($("#filters-content").classList.contains("hidden")) {
-    $("#filters-content").classList.remove("hidden");
-    $("#hide-show-filters").innerText = "Ocultar filtros";
-  } else {
-    $("#filters-content").classList.add("hidden");
-    $("#hide-show-filters").innerText = "Mostrar filtros";
+const hideElement = (selectors) => {
+  for (const selector of selectors) {
+    $(selector).classList.add("hidden");
   }
-});
+};
+
+const cleanContainer = (selector) => {
+  $(selector).innerHTML = "";
+};
+
+const randomId = () => self.crypto.randomUUID();
+
+const setInfo = (key, info) => localStorage.setItem(key, JSON.stringify(info));
+const getInfo = (key) => JSON.parse(localStorage.getItem(key));
